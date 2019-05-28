481ff5f3b7c226c576013ef41f474c2c857a1b4e
luisenricke <luisevm24@outlook.com>
2019-05-28 02:23:51 -0500

    refactor: se actualizo las funciones quitando el parametro del contexto

--- app/src/main/java/com/desarollo/luisvillalobos/gardenkit/Model/Device.kt
+++ app/src/main/java/com/desarollo/luisvillalobos/gardenkit/Model/Device.kt
@@ -51,7 +51,7 @@ class Device {
     }
 
     companion object {
-        const val TABLE_NAME = "DeviceJ"
+        const val TABLE_NAME = "Device"
 
         const val COLUMN_ID = "_id"
         const val COLUMN_NAME = "name"
@@ -72,26 +72,23 @@ class Device {
                 ")"
                 )
 
-        fun fillTable(context: Context) {
-            DBHelper.openDB(context)
-            //Inizialize table
-            createDevice(context,
+        fun fillTable() {
+            //Initialize table
+            createDevice(
                     Device("Dispositivo 1",
                             "device@dev_bitbot_test.dev_bitbot_test",
                             "d8a08f2ed52ec23463402769c3b0ccb6a8e4c6fa4bb6ea77f320cdbf553a2521",
                             "DEV",
                             3))
-            createDevice(context,
+            createDevice(
                     Device("Dispositivo 2",
                             "device@dev_bitbot_test.dev_bitbot_test",
                             "TEST",
                             "TEST",
                             2))
-
-            DBHelper.closeDB()
         }
 
-        fun createDevice(context: Context, device: Device) {
+        fun createDevice(device: Device) {
             //Check and prepare values
             var newRow = ContentValues()
             newRow.put(COLUMN_NAME, device.name)
@@ -101,7 +98,6 @@ class Device {
             newRow.put(COLUMN_IDUSER, device.idUser)
 
             //Execute SQL statement
-            DBHelper.openDB(context)
             try {
                 DBHelper.database!!.insertWithOnConflict(TABLE_NAME,
                         null,
@@ -110,14 +106,12 @@ class Device {
             } catch (e: SQLiteException) {
                 e.printStackTrace()
             } finally {
-                DBHelper.closeDB()
             }
         }
 
-        fun readDevice(context: Context, id: Int): Device? {
+        fun readDevice(id: Int): Device? {
             var device = Device()
             var cursor: Cursor? = null
-            DBHelper.openDB(context)
             try {
                 cursor = DBHelper.database!!.rawQuery("SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID = ? ", arrayOf(id.toString()))
                 cursor?.moveToFirst()
@@ -131,36 +125,32 @@ class Device {
                 e.printStackTrace()
             } finally {
                 cursor?.close()
-                DBHelper.closeDB()
             }
             return device
         }
 
-        fun readDevice(context: Context, device: Device): String? {
+        fun readDevice(device: Device): String? {
             var id: String? = null
             var cursor: Cursor? = null
-            DBHelper.openDB(context)
             try {
                 cursor = DBHelper.database!!.rawQuery("SELECT * FROM $TABLE_NAME WHERE $COLUMN_NAME = ? AND " +
                         "$COLUMN_DEVICE_REQUEST = ? AND " +
                         "$COLUMN_APIKEY_REQUEST = ? AND " +
                         "$COLUMN_DESCRIPTION = ? AND " +
-                        "$COLUMN_IDUSER = ? ", arrayOf(device.name, device.device_request, device.apikey_request,device.description,device.idUser))
+                        "$COLUMN_IDUSER = ? ", arrayOf(device.name, device.device_request, device.apikey_request, device.description, device.idUser))
                 cursor?.moveToFirst()
                 id = cursor?.getString(0)
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
                 cursor?.close()
-                DBHelper.closeDB()
             }
             return id
         }
 
-        fun readDevices(context: Context): List<Device>? {
+        fun readDevices(): List<Device>? {
             var deviceList = ArrayList<Device>()
             var cursor: Cursor? = null
-            DBHelper.openDB(context)
             try {
                 cursor = DBHelper.database!!.rawQuery("SELECT * FROM $TABLE_NAME ", null)
                 cursor.moveToFirst()
@@ -179,15 +169,13 @@ class Device {
                 e.printStackTrace()
             } finally {
                 cursor?.close()
-                DBHelper.closeDB()
             }
             return deviceList
         }
 
-        fun readDevicesWithUser(context: Context,fk_user:Int): List<Device>? {
+        fun readDevicesWithUser(fk_user: Int): ArrayList<Device>? {
             var deviceList = ArrayList<Device>()
             var cursor: Cursor? = null
-            DBHelper.openDB(context)
             try {
                 cursor = DBHelper.database!!.rawQuery("SELECT * FROM $TABLE_NAME WHERE $COLUMN_IDUSER = ? ", arrayOf(fk_user.toString()))
                 cursor.moveToFirst()
@@ -206,16 +194,13 @@ class Device {
                 e.printStackTrace()
             } finally {
                 cursor?.close()
-                DBHelper.closeDB()
             }
             return deviceList
         }
 
-        // TODO: Validar si hay los mismo campos
-        fun updateDevice(context: Context, device: Device?): Boolean {
-            val deviceCheck: Device? = readDevice(context, device!!.id)
+        fun updateDevice(device: Device?): Boolean {
+            val deviceCheck: Device? = readDevice(device!!.id)
             var values = ContentValues()
-            DBHelper.openDB(context)
             if (device.id == 0 && deviceCheck != null)
                 return false
             if (device.name != null)
@@ -226,51 +211,44 @@ class Device {
                 values.put(COLUMN_APIKEY_REQUEST, device.apikey_request)
             if (device.description != null)
                 values.put(COLUMN_DESCRIPTION, device.description)
-            if (device.idUser!=0)
-                values.put(COLUMN_IDUSER,device.idUser)
+            if (device.idUser != 0)
+                values.put(COLUMN_IDUSER, device.idUser)
             try {
                 return DBHelper.database!!.updateWithOnConflict(TABLE_NAME, values, "$COLUMN_ID = ? ", arrayOf(device.id.toString()), SQLiteDatabase.CONFLICT_ROLLBACK) > 0
             } catch (e: SQLiteException) {
                 e.printStackTrace()
             } finally {
-                DBHelper.closeDB()
             }
             return false
         }
 
-        fun deleteDevice(context: Context, id: Int): Boolean {
-            DBHelper.openDB(context)
+        fun deleteDevice(id: Int): Boolean {
             try {
                 return DBHelper.database!!.delete(TABLE_NAME, "$COLUMN_ID = ? ", arrayOf(id.toString())) > 0
             } catch (e: SQLiteException) {
                 e.printStackTrace()
             } finally {
-                DBHelper.closeDB()
             }
             return false
         }
 
-        fun deleteAllDevices(context: Context): Boolean {
-            DBHelper.openDB(context)
+        fun deleteAllDevices(): Boolean {
             try {
                 return DBHelper.database!!.delete(TABLE_NAME, "$COLUMN_ID > ? ", arrayOf("0")) > 0
             } catch (e: SQLiteException) {
                 e.printStackTrace()
             } finally {
-                DBHelper.closeDB()
             }
             return false
         }
 
-        fun getCount(context: Context): Long {
-            DBHelper.openDB(context)
+        fun getCount(): Long {
             var count: Long = 0
             try {
                 count = DatabaseUtils.queryNumEntries(DBHelper.database!!, TABLE_NAME)
             } catch (e: KotlinNullPointerException) {
                 e.printStackTrace()
             } finally {
-                DBHelper.closeDB()
             }
             return count
         }
