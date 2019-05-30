5242e12a28fa43bf5d5943a8e4007f9df2aeb40c
luisenricke <luisevm24@outlook.com>
2019-05-30 14:24:01 -0500

    feat: se implemento el menu contextual en el listview

--- app/src/main/java/com/desarollo/luisvillalobos/gardenkit/Activity/ListDevices.kt
+++ app/src/main/java/com/desarollo/luisvillalobos/gardenkit/Activity/ListDevices.kt
@@ -8,8 +8,7 @@ import android.database.Cursor
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.*
 import com.desarollo.luisvillalobos.gardenkit.Controller.DBHelper
 import com.desarollo.luisvillalobos.gardenkit.Controller.DeviceAdapter
@@ -20,18 +19,21 @@ import com.desarollo.luisvillalobos.gardenkit.Model.User
 import com.desarollo.luisvillalobos.gardenkit.R
 import kotlinx.android.synthetic.main.list_devices.*
 
-class ListDevices : AppCompatActivity(), View.OnClickListener, AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+class ListDevices : AppCompatActivity(), View.OnClickListener, AdapterView.OnItemClickListener/*, AdapterView.OnItemLongClickListener*/ {
+
+    companion object {
+        val DEVICE_PARCEABLE_TAG = "device"
+    }
 
     private var key: Int = 0
     private lateinit var deviceList: ArrayList<Device>
     private lateinit var deviceAdapter: DeviceAdapter
-    //ADD REQUEST
+    private val REQUEST_FORMDEVICE = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_devices)
-        setup()
-
+        //Check if user is logged
         try {
             key = Integer.parseInt("" + getSharedPreferences(Login.PREFS_NAME, Context.MODE_PRIVATE).getString("user_id", null))
         } catch (e: NumberFormatException) {
@@ -48,47 +50,8 @@ class ListDevices : AppCompatActivity(), View.OnClickListener, AdapterView.OnIte
             startActivity(intent)
         }
 
-        try {
-            //var cursor:Cursor = Device.readDevicesWithUser(key)!!
-            //val deviceAdapter = DeviceCursorAdapter(applicationContext,cursor)
-
-            deviceList = Device.readDevicesWithUser(key)!!
-            deviceAdapter = DeviceAdapter(this, deviceList)
-            lvDevice.adapter = deviceAdapter
-        } catch (ex: Exception) {
-            log("${ex.message}")
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        DBHelper.openDB(baseContext)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        DBHelper.openDB(baseContext)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        DBHelper.closeDB()
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btn_logout -> logOutBtnClick()
-            R.id.btn_add -> addBtnClick()
-            R.id.btn_home -> homeBtnClick()
-        }
-    }
-
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
-    }
-
-    override fun onItemLongClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long): Boolean {
-        return true
+        //SetUp All Activity
+        setup()
     }
 
     private fun setup() {
@@ -101,9 +64,31 @@ class ListDevices : AppCompatActivity(), View.OnClickListener, AdapterView.OnIte
         btn_logout.setOnClickListener(this)
         btn_add.setOnClickListener(this)
         btn_home.setOnClickListener(this)
+        lvDevice.onItemClickListener = this
 
         //Database
         DBHelper.openDB(baseContext)
+
+        //Setting ListView
+        try {
+            //var cursor:Cursor = Device.readDevicesWithUser(key)!!
+            //val deviceAdapter = DeviceCursorAdapter(applicationContext,cursor)
+            deviceList = Device.readDevicesWithUser(key)!!
+            deviceAdapter = DeviceAdapter(this, deviceList)
+            lvDevice.adapter = deviceAdapter
+            registerForContextMenu(lvDevice)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    //ClickListeners buttons implementation
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_logout -> logOutBtnClick()
+            R.id.btn_add -> addBtnClick()
+            R.id.btn_home -> homeBtnClick()
+        }
     }
 
     private fun logOutBtnClick() {
@@ -126,10 +111,62 @@ class ListDevices : AppCompatActivity(), View.OnClickListener, AdapterView.OnIte
 
     private fun homeBtnClick() {}
 
+    //ClickListener from ListView implementation
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val lblName: TextView = view!!.findViewById(R.id.lblUser)
+        var device: Device = Device.Companion.readDevice(lblName.text.toString())!!
+        var intent: Intent = Intent(this, Graphs::class.java)
+        intent.putExtra(DEVICE_PARCEABLE_TAG, device)
+        intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
+        startActivity(intent)
+    }
+
+    // ContexMenu implementation
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        if (v?.id == R.id.lvDevice) {
+            var menuInflater: MenuInflater = menuInflater
+            menuInflater.inflate(R.menu.listview_menu, menu)
+            menu?.setHeaderTitle("Selecciones una operación")
+        }
+    }
+
+    //TODO: make function to delete and modify items
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            R.id.item_modificar -> {
+                log("click modificar")
+                return true
+            }
+            R.id.item_eliminar -> {
+                log("click eliminar")
+                return true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+    //Cycle of life from Activity
+    override fun onStart() {
+        super.onStart()
+        DBHelper.openDB(baseContext)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        DBHelper.openDB(baseContext)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        DBHelper.closeDB()
+    }
+
+    //Extras
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
-            val device: Device? = data?.extras!!.getParcelable("device")
+        if (requestCode == REQUEST_FORMDEVICE && resultCode == Activity.RESULT_OK) {
+            val device: Device? = data?.extras!!.getParcelable(DEVICE_PARCEABLE_TAG)
             DBHelper.openDB(baseContext)
             if (Device.createDevice(device!!)) {
                 deviceAdapter.add(device)
@@ -144,6 +181,7 @@ class ListDevices : AppCompatActivity(), View.OnClickListener, AdapterView.OnIte
         moveTaskToBack(true)
     }
 
+    //Message
     private fun toast(message: String) {
         Toast.makeText(baseContext, "$message", Toast.LENGTH_LONG).show()
     }
